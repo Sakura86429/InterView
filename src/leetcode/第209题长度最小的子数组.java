dea71215d60ac5ebf0681f5a4c54a6f7da278600
zhouh <441237909@qq.com>
2022-08-20 12:54:48 +0800

    滑动窗口

--- src/leetcode/第209题长度最小的子数组.java
+++ src/leetcode/第209题长度最小的子数组.java
@@ -15,14 +15,27 @@ public class 第209题长度最小的子数组 {
     public int minSubArrayLen(int target, int[] nums) {
         int l = 0;
         int sum = 0;
-        int res = Integer.MAX_VALUE;
-        for (int r = 0; r < nums.length; r++) {
-            sum += nums[r];   // 只有+-操作，数组出现负数也没关系
-            while (sum >= target) {   // 使用while，每次更新起始位置i
-                res = Math.min(res, r - l + 1);
-                sum -= nums[l++];   // 只有一个元素的话，l++右边也会跟着动r从0开始r++   // 最大到不满足sum>=target，最接近情况也会到l=r+1的位置，之后r进入for循环+1 正常了
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            while (sum >= target) {   // 要计算到=的情况
+                result = Math.min(result, i - l + 1);
+                sum -= nums[l++];   // l最大到i+1，但是下一步i++sum就等于i++的元素了，不影响
             }
         }
-        return res == Integer.MAX_VALUE ? 0 : res;
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
+//    public int minSubArrayLen(int target, int[] nums) {
+//        int l = 0;
+//        int sum = 0;
+//        int res = Integer.MAX_VALUE;
+//        for (int r = 0; r < nums.length; r++) {
+//            sum += nums[r];   // 只有+-操作，数组出现负数也没关系
+//            while (sum >= target) {   // 使用while，每次更新起始位置i
+//                res = Math.min(res, r - l + 1);
+//                sum -= nums[l++];   // 只有一个元素的话，l++右边也会跟着动r从0开始r++   // 最大到不满足sum>=target，最接近情况也会到l=r+1的位置，之后r进入for循环+1 正常了
+//            }
+//        }
+//        return res == Integer.MAX_VALUE ? 0 : res;
+//    }
 }
