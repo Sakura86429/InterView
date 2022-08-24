d791f624931de51ac839e3edabb25afb576b3002
zhouh <441237909@qq.com>
2022-08-24 15:33:11 +0800

    课程表I、II

---
+++ src/leetcode/课程表II.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Topic
+ * Description
+ *
+ * @author zhouh
+ * @version 1.0
+ * Create by 2022/8/24 15:10
+ */
+public class 课程表II {
+    public static void main(String[] args) {
+
+    }
+    List<List<Integer>> list;   // 只是普通的变量，不在class外面不用定义成静态的
+    int[] visited;
+    boolean isValid = true;
+    int[] res;
+    int index;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] info : prerequisites) {
+            list.get(info[1]).add(info[0]);
+        }
+        visited = new int[numCourses];
+        res = new int[numCourses];
+        index = numCourses - 1;   // 只有定义在此函数外面，才能被所有函数使用
+        for (int i = 0; i < numCourses && isValid; i++) {
+            if (visited[i] == 0) dfs(i);
+        }
+        if (!isValid) return new int[0];
+        return res;
+    }
+
+    private void dfs(int i) {
+        visited[i] = 1;
+        for (int v : list.get(i)) {
+            if (visited[v] == 0) {
+                dfs(v);   // dfs每次出来后isValid要判断是否变化
+                if (!isValid) return;   // 为false，有环
+            } else if (visited[v] == 1) {
+                isValid = false;   // isvalid只有一个地方会改变，如果所有循环走完不被改变就是true
+                return;
+            }
+        }
+        visited[i] = 2;
+        res[index--] = i;
+    }
+}
