98616d0e39396a90a3de6868c370a84dd33043b6
zhouh <441237909@qq.com>
2022-08-30 15:17:15 +0800

    盛水最多的容器

--- src/leetcode/第11题盛水最多的容器.java
+++ src/leetcode/第11题盛水最多的容器.java
@@ -17,9 +17,19 @@ public class 第11题盛水最多的容器 {
         int res = 0;
         while (l < r) {
             res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
-            if (height[l] < height[r]) l++;
-            else r--;
+            if (height[l] > height[r]) r--;
+            else l++;
         }
         return res;
     }
+//    public int maxArea(int[] height) {
+//        int l = 0, r = height.length - 1;
+//        int res = 0;
+//        while (l < r) {
+//            res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+//            if (height[l] < height[r]) l++;
+//            else r--;
+//        }
+//        return res;
+//    }
 }
