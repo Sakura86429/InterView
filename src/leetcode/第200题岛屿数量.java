86878381b59c2c80193722cc8fdc92c1f16b8eaa
zhouh <441237909@qq.com>
2022-09-10 15:10:37 +0800

    全排列和岛屿数量

--- src/leetcode/第200题岛屿数量.java
+++ src/leetcode/第200题岛屿数量.java
@@ -10,35 +10,58 @@ package leetcode;
  */
 public class 第200题岛屿数量 {
     public static void main(String[] args) {
-
+        int[][] arr = new int[][] {
+                {}
+        };
     }
+//    public int numIslands(char[][] grid) {
+//        if (grid == null || grid.length == 0) return 0;
+//        int nr = grid.length;
+//        int nc = grid[0].length;
+//        int numIslands = 0;
+//        for (int r = 0; r < nr; r++) {
+//            for (int c = 0; c < nc; c++) {
+//                if (grid[r][c] == '1') {
+//                    numIslands++;
+//                    dfs(grid, r, c);
+//                }
+//            }
+//        }
+//        return numIslands;
+//    }
+//
+//    private void dfs(char[][] grid, int r, int c) {
+//        int nr = grid.length;
+//        int nc = grid[0].length;
+//        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+//            return;
+//        }
+//        grid[r][c] = '0';
+//        dfs(grid, r - 1, c);
+//        dfs(grid, r + 1, c);
+//        dfs(grid, r, c - 1);
+//        dfs(grid, r, c + 1);
+//    }
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        int nr = grid.length;
-        int nc = grid[0].length;
+        if (grid.length == 0 || grid[0].length == 0) return 0;
         int numIslands = 0;
-        for (int r = 0; r < nr; r++) {
-            for (int c = 0; c < nc; c++) {
-                if (grid[r][c] == '1') {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {   // 字符和数字都用==
                     numIslands++;
-                    dfs(grid, r, c);
+                    dfs(grid, i, j);
                 }
             }
         }
         return numIslands;
     }
 
-    private void dfs(char[][] grid, int r, int c) {
-        int nr = grid.length;
-        int nc = grid[0].length;
-        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-            return;
-        }
-        grid[r][c] = '0';
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
+    private void dfs(char[][] grid, int i, int j) {
+        if (i >= grid.length || i < 0 || j >= grid[0].length || j < 0 || grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
-
 }
--- src/leetcode/第46全排列.java
+++ src/leetcode/第46全排列.java
@@ -15,7 +15,8 @@ import java.util.List;
  */
 public class 第46全排列 {
     public static void main(String[] args) {
-        permute(new int[]{1,2,3});
+
+        System.out.println(new 第46全排列().permute(new int[]{1,2,3}));
     }
 //    public static List<List<Integer>> permute(int[] nums) {
 //        List<List<Integer>> ans = new ArrayList<>();
@@ -38,22 +39,62 @@ public class 第46全排列 {
 //        }
 //    }
 
-    public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new LinkedList<>();
-        List<Integer> list = new LinkedList<>();
-        for (int num : nums) {
-            list.add(num);
-        }
-        dfs(ans, list, nums.length, 0);
-        return ans;
+//    public static List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> ans = new LinkedList<>();
+//        List<Integer> list = new LinkedList<>();
+//        for (int num : nums) {
+//            list.add(num);
+//        }
+//        dfs(ans, list, nums.length, 0);
+//        return ans;
+//    }
+//
+//    private static void dfs(List<List<Integer>> ans, List<Integer> list, int n, int index) {
+//        if (index == n) ans.add(new ArrayList<>(list));   // ArrayList加进ans   List<List<Intrger>>
+//        for (int i = index; i < n; i++) {
+//            Collections.swap(list, i, index);
+//            dfs(ans, list, n, index + 1);
+//            Collections.swap(list, i, index);   // 方便for循环的下一个i交换不同元素
+//        }
+//    }
+
+//    public List<List<Integer>> permute(int[] nums) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        List<Integer> sub = new ArrayList<>();
+//        for (int num : nums) sub.add(num);
+//        dfs(res, sub, sub.size(), 0);
+//        return res;
+//    }
+//
+//    private void dfs(List<List<Integer>> res, List<Integer> sub, int length, int index) {
+//        if (index == length - 1) {   // 交换完1下标，+1=2的时候就可以收了
+////            res.add(sub);   // 使用这个的时候不会有任何变化，会一直添加[1, 2, 3]
+//            res.add(new ArrayList<>(sub));   // 传入参数add的时候，add的加是真的添加进去了，与形参无关
+//            return;
+//        }
+//        for (int i = index; i < length; i++) {   // i从index开始往后交换
+//            Collections.swap(sub, index, i);
+//            dfs(res, sub, length, index + 1);
+//            Collections.swap(sub, index, i);   // 方便下一次for循环交换元素
+//        }
+//    }
+    public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> list = new ArrayList<>();
+            for (int n : nums) list.add(n);
+            dfs(res, list, nums.length, 0);
+            return res;
     }
 
-    private static void dfs(List<List<Integer>> ans, List<Integer> list, int n, int index) {
-        if (index == n) ans.add(new ArrayList<>(list));   // ArrayList加进ans   List<List<Intrger>>
-        for (int i = index; i < n; i++) {
-            Collections.swap(list, i, index);
-            dfs(ans, list, n, index + 1);
-            Collections.swap(list, i, index);   // 方便for循环的下一个i交换不同元素
+    private void dfs(List<List<Integer>> res, List<Integer> sub, int length, int index) {
+        if (index == length - 1) {
+            res.add(new ArrayList<>(sub));
+            return;
+        }
+        for (int i = index; i < length; i++) {
+            Collections.swap(sub, index, i);
+            dfs(res, sub, length, index + 1);
+            Collections.swap(sub, index, i);
         }
     }
 }
